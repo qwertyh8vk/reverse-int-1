36417e4535c08c0cef1e2034c2cb4bd3b4595d30
= <egorpetrov5663@gmail.com>
2022-08-08 22:11:32 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function reverse (n) {
-  
+    let stringNum = n + ''
+    let numArr = stringNum.split('')
+    let revesredNumArr = []
+    let revesredStringNum = ''
+    for (let element of numArr){
+        if (element == "0" && numArr[numArr.length-1] == element
+        || element == "-"){
+            //pass
+        }
+        else{
+            revesredNumArr.unshift(element)
+        }
+    }
+    for (let element of revesredNumArr){
+        revesredStringNum += element
+    }
+
+    return Number(revesredStringNum)
+
 }
